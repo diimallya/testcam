a87b4f69b45b729884fb7320063c7b188d24ca10
Vijay <svijayab@gmail.com>
2020-04-22 11:47:22 +0000

    create micro instance on AWS

---
+++ AWS_Simple/createmicroec2/README.md
@@ -0,0 +1,8 @@
+## createmicroec2 infrastructure
+
+Created 4/22/20 by vijay. for createmicroec2
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ AWS_Simple/createmicroec2/variables.tf
@@ -0,0 +1,31 @@
+#####################################################################
+##
+##      Created 4/22/20 by vijay. for createmicroec2
+##
+#####################################################################
+
+variable "aws_instance_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
