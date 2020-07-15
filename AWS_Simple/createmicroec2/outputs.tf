1c7b00c0a4cb0796a2e3eacac563bca69128f0ef
Divya Kamath <divya.mallya86@gmail.com>
2020-07-15 18:17:05 +0530

    Update outputs.tf

--- AWS_Simple/createmicroec2/outputs.tf
+++ AWS_Simple/createmicroec2/outputs.tf
@@ -5,7 +5,7 @@
 #####################################################################
 
 output "vm_public_ip" {
-  value = "Private IP : ${aws_instance.aws_instance.public_ip}"
+  value = "${aws_instance.aws_instance.public_ip}"
 }
 
 output "sample_message" {
